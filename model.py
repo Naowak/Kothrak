7c7bb875a0d34eb329fa60d015ba666fb06f6638
Naowak <yannis.bendiouis@gmail.com>
2019-05-25 16:01:27 +0200

    remove comment

--- model.py
+++ model.py
@@ -52,61 +52,6 @@ class Cell :
             return False
         return True
 
-    # def is_click_in_corner_img(self, click_x, click_y) :
-    #     p, q = (Cell.POS_CELL_CORNER_LEFT, Cell.POS_CELL_CORNER_UP)
-    #     coef = (q[1] - p[1])/(q[0] - p[0])
-    #     b = p[1] - coef*p[0]
-    #     if coef*click_x + b - click_y > 0 :
-    #         return "upleft"
-    #     p, q = (Cell.POS_CELL_CORNER_UP, Cell.POS_CELL_CORNER_RIGHT)
-    #     coef = (q[1] - p[1])/(q[0] - p[0])
-    #     b = p[1] - coef*p[0]
-    #     if coef*click_x + b - click_y > 0 :
-    #         return "upright"
-    #     p, q = (Cell.POS_CELL_CORNER_DOWN, Cell.POS_CELL_CORNER_RIGHT)
-    #     coef = (q[1] - p[1])/(q[0] - p[0])
-    #     b = p[1] - coef*p[0]
-    #     if coef*click_x + b - click_y < 0 :
-    #         return "downright"
-    #     p, q = (Cell.POS_CELL_CORNER_DOWN, Cell.POS_CELL_CORNER_RIGHT)
-    #     coef = (q[1] - p[1])/(q[0] - p[0])
-    #     b = p[1] - coef*p[0]
-    #     if coef*click_x + b - click_y < 0 :
-    #         return "downleft"
-    #     return False
-
-    # def click_cell(self, event) :
-    #     click_x, click_y = (event.pos().x(), event.pos().y())
-    #     corner = self.is_click_in_corner_img(click_x, click_y)
-    #     if corner :
-    #         correct_cell = self.correct_click_cell(corner)
-    #         if correct_cell != None :
-    #             correct_cell.grew()
-    #     else :
-    #         self.grew()
-    
-    # def correct_click_cell(self, corner) :
-    #     if corner == "upleft" :
-    #         x = self.x
-    #         y = self.y - 1
-    #     elif corner == "upright" :
-    #         x = self.x + 1
-    #         y = self.y
-    #     elif corner == "downright" :
-    #         x = self.x 
-    #         y = self.y + 1
-    #     elif corner == "downleft" :
-    #         x = self.x - 1
-    #         y = self.y
-    #     else :
-    #         raise Exception("Incorrect corner.")
-    #     # PEUT ETRE AMELIORER
-    #     cell = None
-    #     for c in self.app.cells :
-    #         if c.x == x and c.y == y :
-    #             cell = c
-    #     return cell
-
     def grew(self) :
         if self.height >= 3 :
             print("Cannot grow anymore.")
@@ -146,7 +91,6 @@ class Cell :
         self.img.setPixmap(QtGui.QPixmap("cell{}_resized.png".format(self.height)))
         self.img.setScaledContents(True)
         self.img.setObjectName("cell_{}_{}".format(abs, ord))
-        # self.img.mouseReleaseEvent=lambda event:self.click_cell(event)
 
 
 class MyApp() :
@@ -181,7 +125,7 @@ class MyApp() :
                 if cell.is_pos_in_cell(relative_x, relative_y) :
                     cell.grew()
                     return
-                    
+
     def get_cell(self, abs, ord) :
         for c in self.cells :
             if c.x == abs and c.y == ord :
