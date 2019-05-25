e89a9a862e1c80b26b26eb99d5864f2fd5c072ca
Naowak <yannis.bendiouis@gmail.com>
2019-05-25 19:23:32 +0200

    Mise en place du coef_resize

--- model.py
+++ model.py
@@ -11,12 +11,64 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 import sys
 
+COEF_RESIZE = 0.5
+
+class MyApp() :
+
+
+    TRANSLATE_X = (162 * COEF_RESIZE, -96 * COEF_RESIZE)
+    TRANSLATE_Y = (162 * COEF_RESIZE, 96 * COEF_RESIZE)
+    SIZE_STAGE = 60 * COEF_RESIZE
+    SIZE_CELL_X = 312 * COEF_RESIZE
+    SIZE_CELL_Y = [240 * COEF_RESIZE + 60 * COEF_RESIZE * i for i in range(4)]
+    POS_INITIAL_X = 100 * COEF_RESIZE
+    POS_INITIAL_Y = [500 * COEF_RESIZE - 60 * COEF_RESIZE * i for i in range(4)]
+
+    NB_CELLS = 5
+
+    def __init__(self) :
+        self.window = QWidget()
+        self.window.resize(2000 * COEF_RESIZE, 1200 * COEF_RESIZE)
+        self.window.setWindowTitle('MyApp')
+        self.window.mouseReleaseEvent=lambda event:self.click(event)
+
+        self.cells = list()
+        for i in reversed(list(range(MyApp.NB_CELLS))) :
+            for j in range(MyApp.NB_CELLS) :
+                self.add_cell(i, j)
+
+    def click(self, event) :
+        click_x, click_y = (event.pos().x(), event.pos().y())
+        for i in range(MyApp.NB_CELLS) :
+            for j in reversed(list(range(MyApp.NB_CELLS))) :
+                cell = self.get_cell(i, j)
+                relative_x = click_x - cell.pos_x
+                relative_y = click_y - cell.pos_y
+                if cell.is_pos_in_cell(relative_x, relative_y) :
+                    cell.grew()
+                    return
+
+    def get_cell(self, abs, ord) :
+        for c in self.cells :
+            if c.x == abs and c.y == ord :
+                return c
+
+    def add_cell(self, abs, ord) :
+        cell = Cell(abs, ord, self)
+        self.cells += [cell]
+
+    def show(self) :
+        self.window.show()
+
+
 class Cell :
 
-    POS_CELL_CORNER_LEFT = (0,45)
-    POS_CELL_CORNER_RIGHT = (149,45)
-    POS_CELL_CORNER_UP = (75,0)
-    POS_CELL_CORNER_DOWN = (75,88)
+    POS_CELL_CORNER_LEFT = (0 * COEF_RESIZE,94 * COEF_RESIZE)
+    POS_CELL_CORNER_RIGHT = (311 * COEF_RESIZE,94 * COEF_RESIZE)
+    POS_CELL_CORNER_UP = (156 * COEF_RESIZE,0 * COEF_RESIZE)
+    POS_CELL_CORNER_DOWN = (156 * COEF_RESIZE,183 * COEF_RESIZE)
+
+    MAX_HEIGHT = 3
     
     def __init__(self, x, y, app) :
         self.x = x
@@ -68,7 +120,7 @@ class Cell :
         return True
 
     def grew(self) :
-        if self.height >= 3 :
+        if self.height >= Cell.MAX_HEIGHT :
             print("Cannot grow anymore.")
             return
         self.height += 1
@@ -106,53 +158,6 @@ class Cell :
         self.img.setPixmap(QtGui.QPixmap("cell{}_resized.png".format(self.height)))
         self.img.setScaledContents(True)
         self.img.setObjectName("cell_{}_{}".format(abs, ord))
-
-
-class MyApp() :
-
-    TRANSLATE_X = (78, -46)
-    TRANSLATE_Y = (78, 46)
-    SIZE_STAGE = 29
-    SIZE_CELL_X = 150
-    SIZE_CELL_Y = [116 + 29*i for i in range(4)]
-    POS_INITIAL_X = 50
-    POS_INITIAL_Y = [242 - 29*i for i in range(4)]
-
-    NB_CELLS = 5
-
-    def __init__(self) :
-        self.window = QWidget()
-        self.window.resize(1000, 600)
-        self.window.setWindowTitle('MyApp')
-        self.window.mouseReleaseEvent=lambda event:self.click(event)
-
-        self.cells = list()
-        for i in reversed(list(range(MyApp.NB_CELLS))) :
-            for j in range(MyApp.NB_CELLS) :
-                self.add_cell(i, j)
-
-    def click(self, event) :
-        click_x, click_y = (event.pos().x(), event.pos().y())
-        for i in range(MyApp.NB_CELLS) :
-            for j in reversed(list(range(MyApp.NB_CELLS))) :
-                cell = self.get_cell(i, j)
-                relative_x = click_x - cell.pos_x
-                relative_y = click_y - cell.pos_y
-                if cell.is_pos_in_cell(relative_x, relative_y) :
-                    cell.grew()
-                    return
-
-    def get_cell(self, abs, ord) :
-        for c in self.cells :
-            if c.x == abs and c.y == ord :
-                return c
-
-    def add_cell(self, abs, ord) :
-        cell = Cell(abs, ord, self)
-        self.cells += [cell]
-
-    def show(self) :
-        self.window.show()
     
     
 
