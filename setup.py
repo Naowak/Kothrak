9705b1c5ecab78f977c3d5936ed84e5e6156b910
Yannis Bendi-Ouis <yannis.bendiouis@gmail.com>
2021-03-12 13:04:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='kothrak',
-      version='0.0.1',
+      version='2.1.1',
       install_requires=['gym', 'numpy', 'tensorflow', 'PyQt5', 'torch']  # And any other dependencies foo needs
       )
