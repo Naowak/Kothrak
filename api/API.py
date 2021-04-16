4b57c8050c0cef5b67d0102970ceb6843dc3a6d7
Yannis Bendi-Ouis <yannis.bendiouis@gmail.com>
2021-04-16 17:33:13 +0200

    games and trainings delete after finished

--- api/API.py
+++ api/API.py
@@ -18,8 +18,9 @@ agents = load_all_agents()
 
 @api.route('/agents_infos', methods=['GET'])
 def agents_infos():
-    agents = load_all_agents()  # DON'T LOAD IT IF ALREADY DONE
-    data = {'names': list(agents.keys())}
+    global agents
+    new_agents = load_all_agents(agents)
+    data = {'names': list({**agents, **new_agents}.keys())}
     return cors(data)
 
 
@@ -41,7 +42,10 @@ def agent_play():
     state = torch.tensor(state, device=device).view(1, -1)
 
     action = agents[agent_name].play(state)
-    _, _, done, infos = games[gid].step(action)  # GAMES NEEDS TO BE KILLED
+    _, _, done, infos = games[gid].step(action)
+
+    if done:
+        games.remove(gid)
 
     data = {'gid': gid, **infos}
     print(data)
@@ -53,21 +57,28 @@ def human_play():
     gid, move, build = retrieve_args(gid=int, move='cell', build='cell')
     action = transform_actions_into_number(move, build)
     _, _, done, infos = games[gid].step(action)
+
+    if done:
+        games.remove(gid)
+
     data = {'gid': gid, **infos}
     return cors(data)
 
 
 @api.route('/train', methods=['GET'])
-def train():  # Il faudra la mettre dans un truc qui permet de parallèliser
+def train():
     nb_agents, grid_ray = retrieve_args(nb_agents=int, grid_ray=int)
     env = KothrakEnv(nb_agents, grid_ray)
-    trainer = Trainer(env)
-
-    thread = Thread(target=trainer.run)
-    session = Training(trainer=trainer, thread=thread)
+    session = Training()
+    session.trainer = Trainer(env)
     tid = trainings.add(session)
 
-    thread.start()  # THREAD AND TRAINERS NEEDS TO BE KILLED
+    def task(trainer, tid):
+        trainer.run()
+        trainings.remove(tid)
+
+    session.thread = Thread(target=task, args=(session.trainer, tid))
+    session.thread.start()
 
     data = {'tid': tid, 'status': 'start'}
     return cors(data)
@@ -86,3 +97,14 @@ def watch_training():
 
 if __name__ == "__main__":
     api.run(debug=True)
+
+    # Error managing :
+    # Games and trainings disapear, it should return a message on the page instead
+    # of an error 500
+    # On the godot part : 
+    # - not ask to replay if training is over
+    # - make him play against an ai correctly by choosing an opponent in the list
+    # then :
+    # - pass to multi task (12 actions 2*6, sum of loss)
+    # - a character loose if no play possible (empty list)
+    # - blender work (color tower), create character
--- api/Utils.py
+++ api/Utils.py
@@ -1,6 +1,5 @@
 from flask import request, jsonify
 import os
-from collections import namedtuple
 
 from dqn.Agent import load_agent
 
@@ -23,7 +22,10 @@ class Manager():
         return self.items[id_]
 
 
-Training = namedtuple('Training', ('trainer', 'thread'))
+class Training():
+    def __init__(self, trainer=None, thread=None):
+        self.trainer = trainer
+        self.thread = thread
 
 
 def retrieve_args(**args):
@@ -50,7 +52,7 @@ def cors(data):
     return response
 
 
-def load_all_agents(saves_directory='saves/'):
+def load_all_agents(existing_agents={}, saves_directory='saves/'):
     # Verify path
     if not os.path.exists(saves_directory):
         print(f'No directory {saves_directory}, no saves available.')
@@ -62,8 +64,9 @@ def load_all_agents(saves_directory='saves/'):
     # Load all agents
     agents = {}
     for filename in os.listdir(saves_directory):
-        path = saves_directory + filename
-        agent = load_agent(path)
-        agents[agent.name] = agent
+        if filename[:-4] not in existing_agents.keys():
+            path = saves_directory + filename
+            agent = load_agent(path)
+            agents[agent.name] = agent
 
     return agents
